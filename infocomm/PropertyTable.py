92639bdfbdd0a767df4892ce08c023f3df779c4c
Gavin Andrews <gavinandrews@yahoo.com>
2024-09-04 19:23:06 +0100

    More tidying

--- infocomm/PropertyTable.py
+++ infocomm/PropertyTable.py
@@ -41,33 +41,6 @@ class PropertyTable:
             space = " " * (32 - 3 * l)
             print(f"{space}{n:3}/{l}  (PROP#{n})")
 
-    def get_property_table_entry_after_property_number(self, property_number):
-        property = self.find_first_property()
-        if property_number == 0:
-            return property
-
-        while property is not None:
-            if property.get_property_number() == property_number:
-                return self.find_next_property(property)
-            if property.get_property_number() < property_number:
-                return None
-            property = self.find_next_property(property)
-
-        return None
-
-    def get_property_table_entry_for_property_number(self, property_number):
-
-        property = self.find_first_property()
-        while property is not None:
-            if property.get_property_number() == property_number:
-                return property
-            if property.get_property_number() < property_number:
-                return None
-            property = self.find_next_property(property)
-
-        return None
-
-
     def find_first_property(self):
 
         # Skip Description
@@ -86,20 +59,46 @@ class PropertyTable:
 
     def find_next_property(self, previous):
 
-            paddr = previous.get_address()
+        paddr = previous.get_address()
 
-            # Advance to next one
-            n, l = PropertyTableEntry.decode_n_and_l(self.memory[paddr])
-            paddr += 1
-            paddr += l
+        # Advance to next one
+        n, l = PropertyTableEntry.decode_n_and_l(self.memory[paddr])
+        paddr += 1
+        paddr += l
 
-            prop = self.memory[paddr]
+        prop = self.memory[paddr]
 
-            n, l = PropertyTableEntry.decode_n_and_l(prop)
-            if n != 0:
-                return PropertyTableEntry(paddr, self.memory)
-            else:
+        n, l = PropertyTableEntry.decode_n_and_l(prop)
+        if n != 0:
+            return PropertyTableEntry(paddr, self.memory)
+        else:
+            return None
+
+    def get_property_table_entry_for_property_number(self, property_number):
+
+        property = self.find_first_property()
+        while property is not None:
+            if property.get_property_number() == property_number:
+                return property
+            if property.get_property_number() < property_number:
                 return None
+            property = self.find_next_property(property)
+
+        return None
+
+    def get_property_table_entry_after_property_number(self, property_number):
+        property = self.find_first_property()
+        if property_number == 0:
+            return property
+
+        while property is not None:
+            if property.get_property_number() == property_number:
+                return self.find_next_property(property)
+            if property.get_property_number() < property_number:
+                return None
+            property = self.find_next_property(property)
+
+        return None
 
     def get_property_table_entry_address(self, property_number):
         property_entry = self.get_property_table_entry_for_property_number(property_number)
@@ -110,4 +109,3 @@ class PropertyTable:
 
     def get_description(self):
         return self.description()
-
